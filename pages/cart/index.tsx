c9532e8c9bca0ac3a3cfdd3318184b2f476b9e0a
reemrev21 <reemrev21@gmail.com>
2023-01-09 21:05:17 +0900

    [feature] #5 input css reset 추가 - cart input style 을 위함

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
+import styled from "styled-components";
+import Card from "../../src/components/cart/Card";
+import { productItems } from "../../src/__mocks__/productsItems";
+import { productItem } from "../../src/types/product";
 
-export default function Home() {
+function Cart() {
   return (
     <>
       <Head>
@@ -10,8 +14,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>cart</div>
+        <Wrapper>
+          <div>
+            <h2>상품</h2>
+            {productItems.map((item: productItem) => (
+              <Card
+                key={item.item_no}
+                item_no={item.item_no}
+                item_name={item.item_name}
+                detail_image_url={item.detail_image_url}
+                price={item.price}
+                score={item.score}
+              />
+            ))}
+          </div>
+        </Wrapper>
       </main>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 0 16px;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-row-gap: 20px;
+`;
+
+export default Cart;
